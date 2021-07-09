08ed2fa4b4f04f28fc8e82c88c344a5b0e7200f4
Philipp <filshe1512@gmail.com>
2021-07-09 22:32:01 +0300

    add some methods and constructors

--- stl_impl.h
+++ stl_impl.h
@@ -2,24 +2,40 @@
 #ifndef __STL_IMPL__
 #define __STL_IMPL__
 
-
+//Имплементация свой библиотеки на основе stl(для обучения и получения опыта)
+//Пока что только для контейнеров и вспомогательных классов
 namespace stl_impl {
 
+    
+
+    //Имплементация умного указателя(пока что без Deleter и специализации на T[])
     template<typename T>
     class unique_ptr{
     private:
         T* ptr;
     public:
-        unique_ptr() noexcept {}
-        unique_ptr(T new_ptr) noexcept:
-        ptr(new_ptr) {}
-        unique_ptr(unique_ptr&& move_ptr){
-
+        unique_ptr() noexcept : ptr(nullptr) {}
+        unique_ptr(T* new_ptr) noexcept : ptr(new_ptr) {}
+        unique_ptr(unique_ptr&& move_ptr) {
+            ptr = move_ptr.ptr;
+            move_ptr.ptr = nullptr;
         }
         ~unique_ptr(){
-            delete ptr;
+            if (get() == nullptr){
+
+            } else {
+                delete ptr;
+                ptr = nullptr;
+            }
+            
         }
-        T get() const noexcept{
+
+        unique_ptr& operator=(unique_ptr&& move_ptr){
+            ptr = move_ptr.ptr;
+            move_ptr.ptr = nullptr;
+            return *this;
+        }
+        T* get() const noexcept{
             return ptr;
         }
         T& operator*(){
@@ -28,6 +44,19 @@ namespace stl_impl {
         T* operator->(){
             return ptr;
         }
+        T* release() noexcept{
+            T* temp_ptr = ptr;
+            ptr = nullptr;
+            return temp_ptr;
+        }
+        friend bool operator==(const unique_ptr<T>& ptr1, const unique_ptr<T>& ptr2){
+            return *ptr1.get() == *ptr2.get() ? true : false;
+        }
+
+        friend bool operator!=(const unique_ptr<T>& ptr1, const unique_ptr<T>& ptr2){
+            return *ptr1.get() != *ptr2.get() ? true : false;
+        }
+        
 
 
     };
@@ -37,10 +66,10 @@ namespace stl_impl {
     class forward_list {
     private:
         struct node:  {
-            node* pNext;
+            unique_ptr<node> pNext;
             T data;
             node(){}
-            node(T data = T(), pNext = nullptr)
+            node(T new_data = T(), unique_ptr<node> new_pNext = nullptr) : data{new_data} , pNext{new_pNext}
         };
         node* head;
     public:
